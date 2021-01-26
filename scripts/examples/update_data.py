4d2b63a620b4622666d0e1d4093df2c14b796df5
Gabriel Pena <gpena@untref.edu.ar>
2021-01-26 17:45:41 -0300

    Adding unversioned files

---
+++ scripts/examples/update_data.py
@@ -0,0 +1,5 @@
+from src.data_io.data_initializer import update_data
+
+# This will retrieve the data from the default source and save it in a csv file for further use
+
+update_data()
---
+++ src/interface/epydemics.py
@@ -0,0 +1,27 @@
+from src.data_io.console_manager import ConsoleManager
+from src.data_io.data_manager import DataManager
+from src.data_io.plot_manager import PlotManager
+from src.fitters.fitter import Fitter
+
+console = ConsoleManager.get_instance()
+plotter = PlotManager.get_instance()
+
+
+def show_available_countries():
+    countries = DataManager.get_country_list()
+    console.print_countries(countries)
+
+
+def show_data_from_country(country_id, dataset='total_cases'):
+    country_data = DataManager.get_country_data(country_id, dataset)
+    console.print_data_from_country(country_data, country_id, dataset)
+
+
+def fit_contagion_model(country_name, dataset='total_cases', start=1, end=-1, output=True, plot=True):
+    fit = Fitter.fit(country_name, dataset, start, end)
+
+    if output is True:
+        console.show_fit_results(fit)
+        
+    if plot is True:
+        plotter.plot_fit_results(fit)
