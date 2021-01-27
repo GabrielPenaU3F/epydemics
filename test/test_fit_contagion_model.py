36168b81798fcbf19a4ae7c2319951b284851198
Gabriel Pena <gpena@untref.edu.ar>
2021-01-27 02:21:52 -0300

    Fit contagion model tests added

---
+++ test/test_fit_contagion_model.py
@@ -0,0 +1,43 @@
+import unittest
+
+from src.data_io.data_manager import DataManager
+from src.fitters.fitter import Fitter
+
+
+class CountryListTests(unittest.TestCase):
+
+    arg_early_fit = None
+    arg_mitigation_fit = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        DataManager.load_dataset()
+        cls.arg_early_fit = Fitter.fit('Argentina', 'total_cases', 1, 200)
+        cls.arg_mitigation_fit = Fitter.fit('Argentina', 'total_cases', 200, 300)
+
+    def test_argentina_early_stage_rho_parameter_should_be_0_dot_130(self):
+        rho = self.__class__.arg_early_fit.get_params()[0]
+        self.assertAlmostEqual(0.130, rho, places=3)
+
+    def test_argentina_early_stage_gamma_by_rho_parameter_should_be_4_dot_501(self):
+        gamma_by_rho = self.__class__.arg_early_fit.get_params()[1]
+        self.assertAlmostEqual(4.502, gamma_by_rho, places=3)
+
+    def test_argentina_early_stage_rsq_should_be_0_dot_999(self):
+        rsq = self.__class__.arg_early_fit.get_rsq()
+        self.assertAlmostEqual(0.999, rsq, places=3)
+
+    def test_argentina_mitigation_stage_rho_parameter_should_be_216070(self):
+        rho = self.__class__.arg_mitigation_fit.get_params()[0]
+        self.assertAlmostEqual(216070, rho, places=0)
+
+    def test_argentina_mitigation_stage_gamma_by_rho_parameter_should_be_0_dot_807(self):
+        gamma_by_rho = self.__class__.arg_mitigation_fit.get_params()[1]
+        self.assertAlmostEqual(0.807, gamma_by_rho, places=3)
+
+    def test_argentina_mitigation_stage_rsq_should_be_0_dot_987(self):
+        rsq = self.__class__.arg_mitigation_fit.get_rsq()
+        self.assertAlmostEqual(0.987, rsq, places=3)
+
+    if __name__ == '__main__':
+        unittest.main()
