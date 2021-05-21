32a0475237b1dde4436dbc0b8d6efaf2305d5756
Gabriel Pena <gpena@untref.edu.ar>
2021-05-20 23:52:21 -0300

    Added MTBI inverse tests

--- test/system/test_calculate_mtbi_inverse_owid.py
+++ test/system/test_calculate_mtbi_inverse_owid.py
@@ -6,50 +6,43 @@ from src.data_manipulation.data_manager import DataManager
 import src.interface.epydemics as ep
 
 
-class CalculateMTBIInverseOWIDTest(unittest.TestCase):
+class CalculateMTBIInverseStarwarsTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        DataManager.load_dataset('owid')
+        DataManager.load_dataset(source='custom', filename='starwars_test_data.csv')
 
-    def test_mtbi_inverses_owid_arg_from_1_to_40_starting_from_30(self):
-        expected_inverses = np.power(np.array([0.0069, 0.0069, 0.0067, 0.0063, 0.0064,
-                                               0.0064, 0.0065, 0.0066, 0.0067, 0.0066, 0.0067]), -1)
-        mtbi_inverses = np.array(ep.calculate_mtbi_inverse('Argentina', dataset='total_cases', unit='day',
-                                                           start=1, end=40, start_from=30, fit_x0=(0.1, 1),
+    def test_mtbi_inverses_coruscant_from_1_to_40_starting_from_30(self):
+        expected_inverses = np.power(np.array([0.0107, 0.0106, 0.0106, 0.0106, 0.0107,
+                                               0.0109, 0.0110, 0.0111, 0.0112, 0.0114, 0.0115]), -1)
+        mtbi_inverses = np.array(ep.calculate_mtbi_inverse('Coruscant', end=40, fit_x0=(0.1, 1),
                                                            formula='approx_conditional', output=False))
-        testing.assert_allclose(expected_inverses, mtbi_inverses, rtol=0.1)
+        testing.assert_allclose(expected_inverses, mtbi_inverses, rtol=0.01)
 
-    '''
-    def test_mtbi_inverses_owid_arg_from_40_to_80(self):
-        expected_inverses = np.power(np.array([0.0027, 0.0026, 0.0026, 0.0026, 0.0026,
-                                               0.0026, 0.0025, 0.0025, 0.0025, 0.0025, 0.0025]), -1)
-        mtbi_inverses = np.array(ep.calculate_mtbi_inverse('Argentina', dataset='total_cases', unit='day',
-                                                           start=40, end=80, fit_x0=(0.1, 1),
+    def test_mtbi_inverses_coruscant_day_20_to_60(self):
+        expected_inverses = np.power(np.array([0.0103, 0.0100, 0.0099, 0.0098, 0.0096, 0.0095,
+                                               0.0094, 0.0092, 0.0091, 0.0090, 0.0089, 0.0088]), -1)
+        mtbi_inverses = np.array(ep.calculate_mtbi_inverse('Coruscant', start=20, end=60, fit_x0=(0.1, 1),
                                                            formula='approx_conditional', output=False))
-        testing.assert_allclose(expected_inverses, mtbi_inverses, rtol=0.1)
-    '''
+        testing.assert_allclose(expected_inverses, mtbi_inverses, rtol=0.01)
 
+    def test_mtbi_inverses_coruscant_day_20_to_60_from_35(self):
+        expected_inverses = np.power(np.array([0.0095, 0.0094, 0.0092, 0.0091, 0.0090, 0.0089, 0.0088]), -1)
+        mtbi_inverses = np.array(ep.calculate_mtbi_inverse('Coruscant', start=20, end=60, start_from=35,
+                                                  formula='approx_conditional', output=False))
+        testing.assert_allclose(expected_inverses, mtbi_inverses, rtol=0.01)
 
-    '''
-    def test_mtbi_owid_arg_50_days_from_30(self):
-        expected_mtbis_day = np.array([0.0069, 0.0069, 0.0067, 0.0063, 0.0064,
-                                       0.0064, 0.0065, 0.0066, 0.0067, 0.0066, 0.0067,
-                                       0.0067, 0.0067, 0.0068, 0.0067, 0.0067, 0.0066,
-                                       0.0066, 0.0067, 0.0067, 0.0067])
-        mtbis = np.array(Fitter.calculate_mtbis('Argentina', dataset='total_cases',
-                                                start=1, end=50, start_from=30, fit_x0=(0.1, 1),
-                                                formula='approx_conditional'))
-        testing.assert_array_almost_equal(expected_mtbis_day, mtbis, decimal=4)
+    def test_mtbi_inverses_coruscant_day_30_to_the_end(self):
+        expected_inverses = np.power(np.array([0.0074, 0.0074, 0.0075, 0.0075, 0.0076, 0.0078,
+                                               0.0079, 0.0080, 0.0082, 0.0083, 0.0084]), -1)
+        mtbi_inverses = np.array(ep.calculate_mtbi_inverse('Coruscant', start=30, formula='approx_conditional', output=False))
+        testing.assert_allclose(expected_inverses, mtbi_inverses, rtol=0.01)
 
-    def test_mtbi_owid_arg_50_days_from_40(self):
-        expected_mtbis = np.array([0.0067, 0.0067, 0.0067, 0.0068, 0.0067,
-                                   0.0067, 0.0067, 0.0066, 0.0067, 0.0067, 0.0067])
-        mtbis = np.array(Fitter.calculate_mtbis('Argentina', dataset='total_cases',
-                                                start=1, end=50, start_from=40, fit_x0=(0.1, 1),
-                                                formula='approx_conditional'))
-        testing.assert_array_almost_equal(expected_mtbis, mtbis, decimal=4)
-        '''
+    def test_mtbi_inverses_coruscant_day_30_to_the_end_from_35(self):
+        expected_inverses = np.power(np.array([0.0078, 0.0079, 0.0080, 0.0082, 0.0083, 0.0084]), -1)
+        mtbi_inverses = np.array(ep.calculate_mtbi_inverse('Coruscant', start=30, start_from=35, formula='approx_conditional', output=False))
+        testing.assert_allclose(expected_inverses, mtbi_inverses, rtol=0.01)
+
 
 if __name__ == '__main__':
     unittest.main()
