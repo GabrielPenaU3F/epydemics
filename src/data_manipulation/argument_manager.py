fc1d6939c70a18f5f85031b66d833c8e9e02f301
Gabriel Pena <gpena@untref.edu.ar>
2021-05-25 00:37:34 -0300

    Further unit testing added

--- src/data_manipulation/argument_manager.py
+++ src/data_manipulation/argument_manager.py
@@ -12,6 +12,8 @@ class ArgumentManager:
 
         cls.validate_indexes(actual_dataset_end, start, end)
 
+        return True
+
     @classmethod
     def validate_indexes(cls, actual_dataset_end, start_argument, end_argument):
         if end_argument is not None:
@@ -24,18 +26,21 @@ class ArgumentManager:
         elif start_argument > actual_dataset_end:
             raise InvalidArgumentException('Start argument cannot exceed dataset length')
 
+        return True
 
     @classmethod
     def validate_dataset(cls, source, dataset):
         supported_datasets = source.get_supported_datasets()
         if dataset not in supported_datasets:
             raise InvalidArgumentException('Requested dataset is not supported')
+        return True
 
     @classmethod
     def validate_location(cls, raw_data, location_column_name, location_id):
         current_locations = raw_data[location_column_name].unique().tolist()
         if location_id not in current_locations:
             raise InvalidArgumentException('The requested location was not found')
+        return True
 
     @classmethod
     def validate_source(cls, source):
--- test/unit/test_argument_manager.py
+++ test/unit/test_argument_manager.py
@@ -2,23 +2,99 @@ import unittest
 
 from src.data_manipulation.argument_manager import ArgumentManager
 from src.exceptions.exceptions import InvalidArgumentException
+from src.repository.source_repository import SourceRepository
 
 
 class ArgumentManagerTest(unittest.TestCase):
 
+    # Source validations
+
     def test_owid_source_must_be_supported(self):
-        self.assertEqual(ArgumentManager.validate_source('owid'), True)
+        self.assertTrue(ArgumentManager.validate_source('owid'))
 
     def test_mapache_arg_source_must_be_supported(self):
-        self.assertEqual(ArgumentManager.validate_source('mapache_arg'), True)
+        self.assertTrue(ArgumentManager.validate_source('mapache_arg'))
 
     def test_custom_source_must_be_supported(self):
-        self.assertEqual(ArgumentManager.validate_source('custom'), True)
+        self.assertTrue(ArgumentManager.validate_source('custom'))
 
     def test_ministerio_de_salud_source_must_not_be_supported(self):
         with self.assertRaises(InvalidArgumentException) as error:
             ArgumentManager.validate_source('ministerio_de_salud')
         self.assertEqual(error.exception.strerror, 'The requested source is not supported')
+
+    # Dataset validations
+
+    def test_owid_source_must_support_total_cases_dataset(self):
+        source = SourceRepository.retrieve_data_source('owid')
+        self.assertTrue(ArgumentManager.validate_dataset(source, 'total_cases'))
+
+    def test_owid_source_must_support_total_deaths_dataset(self):
+        source = SourceRepository.retrieve_data_source('owid')
+        self.assertTrue(ArgumentManager.validate_dataset(source, 'total_deaths'))
+
+    def test_owid_source_must_not_support_casos_dataset(self):
+        source = SourceRepository.retrieve_data_source('owid')
+        with self.assertRaises(InvalidArgumentException) as error:
+            ArgumentManager.validate_dataset(source, 'casos')
+        self.assertEqual(error.exception.strerror, 'Requested dataset is not supported')
+
+    def test_mapache_arg_source_must_support_nue_casosconf_diff_dataset(self):
+        source = SourceRepository.retrieve_data_source('mapache_arg')
+        self.assertTrue(ArgumentManager.validate_dataset(source, 'nue_casosconf_diff'))
+
+    def test_mapache_arg_source_must_support_nue_fallecidos_diff_dataset(self):
+        source = SourceRepository.retrieve_data_source('mapache_arg')
+        self.assertTrue(ArgumentManager.validate_dataset(source, 'nue_fallecidos_diff'))
+
+    def test_mapache_arg_source_must_not_support_casos_dataset(self):
+        source = SourceRepository.retrieve_data_source('mapache_arg')
+        with self.assertRaises(InvalidArgumentException) as error:
+            ArgumentManager.validate_dataset(source, 'casos')
+        self.assertEqual(error.exception.strerror, 'Requested dataset is not supported')
+
+    def test_custom_source_must_support_custom_data_dataset(self):
+        source = SourceRepository.retrieve_data_source('custom')
+        self.assertTrue(ArgumentManager.validate_dataset(source, 'custom_data'))
+
+    def test_custom_source_must_not_support_total_deaths_dataset(self):
+        source = SourceRepository.retrieve_data_source('custom')
+        with self.assertRaises(InvalidArgumentException) as error:
+            ArgumentManager.validate_dataset(source, 'total_deaths')
+        self.assertEqual(error.exception.strerror, 'Requested dataset is not supported')
+
+    # Index validations
+
+    def test_dataset_of_length_10_with_start_5_and_end_9_must_be_accepted(self):
+        self.assertTrue(ArgumentManager.validate_indexes(10, 5, 9))
+
+    def test_dataset_of_length_10_with_start_5_and_end_None_must_be_accepted(self):
+        self.assertTrue(ArgumentManager.validate_indexes(10, 5, None))
+
+    def test_dataset_of_length_10_with_start_5_and_end_12_must_raise_exception(self):
+        with self.assertRaises(InvalidArgumentException) as error:
+            ArgumentManager.validate_indexes(10, 5, 12)
+        self.assertEqual(error.exception.strerror, 'End argument cannot exceed dataset length')
+
+    def test_dataset_of_length_10_with_start_12_and_end_14_must_raise_exception(self):
+        with self.assertRaises(InvalidArgumentException) as error:
+            ArgumentManager.validate_indexes(10, 12, 14)
+        self.assertEqual(error.exception.strerror, 'End argument cannot exceed dataset length')
+
+    def test_dataset_of_length_10_with_start_12_and_end_7_must_raise_exception(self):
+        with self.assertRaises(InvalidArgumentException) as error:
+            ArgumentManager.validate_indexes(10, 12, 7)
+        self.assertEqual(error.exception.strerror, 'Start argument cannot exceed end argument')
+
+    def test_dataset_of_length_10_with_start_5_and_end_2_must_raise_exception(self):
+        with self.assertRaises(InvalidArgumentException) as error:
+            ArgumentManager.validate_indexes(10, 5, 2)
+        self.assertEqual(error.exception.strerror, 'Start argument cannot exceed end argument')
+
+    def test_dataset_of_length_10_with_start_12_and_end_None_must_raise_exception(self):
+        with self.assertRaises(InvalidArgumentException) as error:
+            ArgumentManager.validate_indexes(10, 12, None)
+        self.assertEqual(error.exception.strerror, 'Start argument cannot exceed dataset length')
 
 
 if __name__ == '__main__':
