431bd1775cc8b72bf7d2adf591f9cc09191c1dfa
Gabriel Pena <gpena@untref.edu.ar>
2021-10-08 19:54:09 -0300

    removed experiments from master branch

--- scripts/experiments/mtbi_exact_approx.py
+++
@@ -1,8 +0,0 @@
-from src.data_manipulation.data_manager import DataManager
-import src.interface.epydemics as ep
-
-DataManager.load_dataset('owid')
-
-ep.fit_model("United States", start=225, end=360)
-ep.calculate_mtbi("United States", start=225, end=360, unit='day', formula='approx_conditional')
-ep.calculate_mtbi("United States", start=225, end=360, unit='day', formula='exact_conditional')
