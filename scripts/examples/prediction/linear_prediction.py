de120d06dfa2faf72c98ba7bd35f18b60edddf38
Gabriel Pena <gpena@untref.edu.ar>
2021-10-13 19:48:15 -0300

    Example update

--- scripts/examples/prediction/linear_prediction.py
+++ scripts/examples/prediction/linear_prediction.py
@@ -6,22 +6,21 @@ from src.interface import epydemics as ep
 
 DataManager.load_dataset('owid')
 
-country = 'Argentina'
-dataset = 'total_cases'
-start_from = 30
-start = 1
-end = 229
+# We will do a 1-day prediction of the MTBI using a linear regression model
 
-segment_start = 120
-segment_end = 199
+# Step 1: obtain the MTBIS
+mtbis = np.array(ep.calculate_mtbi('Argentina', start=1, end=229, unit='sec',
+                 start_from=30, output=False, formula='approx_conditional'))
 
-mtbis = np.array(ep.calculate_mtbi(country, dataset=dataset, start=start, end=end, unit='sec',
-                 start_from=start_from, output=False, formula='approx_conditional'))
-mtbis_train = mtbis[segment_start:segment_end]
+# Step 2: define the train set. Typically the train set will be the end of the dataset,
+# which allows to predict an unknown value. However, we will perform a comparison with kown data
+mtbis_train = mtbis[120:199]
 
+# Step 3: define the regression parameters. We shall use a model of order K with m samples
 m = 15
 K = 8
 reg = RegressionManager().linear_regression(mtbis, m, K, output='full')
 
+# Step 4: use the model to predict and compare to the real MTBI value
 print('Real MTBI: ' + str(mtbis[199]))
 print('Predicted MTBI: ' + str(reg.predict()))
